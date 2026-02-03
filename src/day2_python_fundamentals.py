efaac509278987d65c3db418cb3fe10fd40f488a
day1 <hareemmadiha3@gmail.com>
2026-02-03 13:10:31 +0530

    day2

---
+++ src/day2_python_fundamentals.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# User input
+
+name = input("Enter your name: ")
+age = int(input("Enter your current age: "))
+
+age_2030 = age+4
+
+print(f"Hey {name}, you will be {age_2030} years old in 2030.")
