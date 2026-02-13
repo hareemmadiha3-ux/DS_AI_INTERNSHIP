ce4394a9346af098989d83693f7fce94fdeeaedf
day1 <hareemmadiha3@gmail.com>
2026-02-13 12:16:44 +0530

    day11

---
+++ src/day11_line_plot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 11:00:45 2026
+
+@author: Madiha
+"""
+
+
+
+import matplotlib.pyplot as plt
+months = [1, 2, 3, 4, 5]
+revenue = [2000, 4500, 4000, 7500, 9000]
+plt.plot(months,revenue)
+plt.xlabel("month")
+plt.ylabel("revenue")
+plt.title("monthly revenue growth")
+plt.show()
+
+
+ 
