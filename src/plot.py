9bf23fee60c069b758352df9ded113cc9a2fdac3
day1 <hareemmadiha3@gmail.com>
2026-02-23 13:31:08 +0530

    updated title in feature-viz

--- src/plot.py
+++ src/plot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4]
-y = [10, 20, 15, 25]
+x = [1, 2, 3, 6]
+y = [10, 20, 15, 27]
 
 plt.plot(x, y)
-plt.title("Experimental Visualization")
+plt.title("Feature Branch Visualization")
 plt.show()
